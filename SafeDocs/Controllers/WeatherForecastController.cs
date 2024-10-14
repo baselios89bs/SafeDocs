151da5f1952d8212097bff23703e91a152e8cc0a
basel.slaby@gmail.com <basel.slaby@gmail.com>
2024-10-14 14:05:23 +0300

    f

--- SafeDocs/Controllers/WeatherForecastController.cs
+++ SafeDocs/Controllers/WeatherForecastController.cs
@@ -8,10 +8,10 @@ namespace SafeDocs.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hott", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> _logger; 
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
